6805402b587219f393a436bddb25bb0b138c100c
abc12354a <abc12354a@hotmail.com>
2017-09-10 22:04:13 +0800

    final edtion

--- Huffman/huffman.cpp
+++ Huffman/huffman.cpp
@@ -5,16 +5,22 @@
 #include "huffman.h"
 #include <iostream>
 #include <cstring>
+//select function remand to fix;
 void Select(HTree Tree, int n, int &s1, int &s2) {
-    s1 = 0;
-    for(int i = 0;i<n;++i){
-        if(Tree[i].weight<Tree[s1].weight&&(Tree[i].parent==0))
-            s1 = i;
-    }
-    s2 = 0;
-    for(int i = 0;i<n;++i){
-        if((Tree[i].weight<Tree[s2].weight)&&i!=s1&&(Tree[i].parent==0))
-            s2 = i;
+    int m1 = 100;
+    int m2 = 100;
+    s1 = s2 = 0;
+    for(int j = 0;j<n;++j){
+        if(Tree[j].weight<m1&&Tree[j].parent == 0){
+            m2 = m1;
+            s2 = s1;
+            m1 = Tree[j].weight;
+            s1 = j;
+        }
+        else if(Tree[j].weight<m2&&Tree[j].parent == 0){
+            m2 = Tree[j].weight;
+            s2 = j;
+        }
     }
 }
 //void Test(my* Tree, int n, int &s1, int &s2) {
@@ -37,7 +43,7 @@ void Huffcode(HTree &Tree, HC &Code, int n) {
 //    auto p = Tree;
     for(int i = 1;i<=n;++i)
     {
-        Tree[i-1].weight = 11-i;
+        Tree[i-1].weight =11-i;
         Tree[i-1].parent =0;
         Tree[i-1].left =0;
         Tree[i-1].right =0;
@@ -46,18 +52,23 @@ void Huffcode(HTree &Tree, HC &Code, int n) {
     {
         Tree[j-1].weight = 0;
         Tree[j-1].parent =0;
-        Tree[j-1].left =0;
+        Tree[j-1].left = 0;
         Tree[j-1].right =0;
     }
-    for(int k = n+1;k<=m;++k){
+    for(int k = n;k<m;++k){
         int s1 = 0;
         int s2 = 0;
         Select(Tree,k,s1,s2);
+        //std::cout<<"第"<<k-n<<"次"<<s1<<" "<<s2<<"  ";
         Tree[s1].parent = k;
         Tree[s2].parent = k;
         Tree[k].weight = Tree[s1].weight+Tree[s2].weight;
         Tree[k].left = s1;
         Tree[k].right = s2;
+    }
+    //std::cout<<std::endl;
+    for(int i = 0;i<=m;i++){
+       // std::cout<<"第"<<i<<"个"<<Tree[i].weight<<" "<<Tree[i].parent<<"   ";
     }
     Code = new char*[n+1];
     auto cd = new char[n];
